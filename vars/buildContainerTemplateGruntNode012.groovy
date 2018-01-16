0f83e25b3b772a18b614d54b4e8793ccf4210134
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-17 10:07:50 +1100

    Try tailing null

--- vars/buildContainerTemplateGruntNode012.groovy
+++ vars/buildContainerTemplateGruntNode012.groovy
@@ -2,7 +2,6 @@ def call() {
 	return containerTemplate(
 		name: 'build-grunt-node012',
 		image: 'agiledigital/build-image-grunt-node012',
-		command: 'cat',
-		ttyEnabled: true
+		command: 'tail -f /dev/null'
 	)
 }
