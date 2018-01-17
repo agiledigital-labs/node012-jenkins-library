f02a0513d82e04c6b3d4cfb0f0204ca3a76f5ae3
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-17 11:56:24 +1100

    Force always pull latest docker image

--- vars/buildContainerTemplateGruntNode012.groovy
+++ vars/buildContainerTemplateGruntNode012.groovy
@@ -2,6 +2,7 @@ def call() {
 	return containerTemplate(
 		name: 'build-grunt-node012',
 		image: 'agiledigital/build-image-grunt-node012',
+        alwaysPullImage: true,
 		command: 'cat',
 		ttyEnabled: true
 	)
