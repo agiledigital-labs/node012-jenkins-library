0f769cd95f03a848912a60a96a426263dba6af98
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-23 16:44:55 +1100

    Rename pipeline methods

--- vars/buildPersistentVolumesGruntNode012.groovy
+++
@@ -1,9 +0,0 @@
-def call() {
-  return [
-    [path: '/home/jenkins/.npm',         sizeGiB: 1],
-    [path: '/home/jenkins/.cache/bower', sizeGiB: 1]
-  ].collect { volume ->
-    volume.name = "${config.project}-${path.replaceAll(/[^a-zA-Z0-9]+/, '-')}"
-    volume
-  }
-}
