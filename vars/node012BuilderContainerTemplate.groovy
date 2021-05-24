53031ea6f8f8ea433fa7cf13e9bb75cd0998284a
Robin McAndrew <rlmcandrew@agiledigital.com.au>
2021-05-24 12:41:03 +1000

    Update node012BuilderContainerTemplate.groovy

--- vars/node012BuilderContainerTemplate.groovy
+++ vars/node012BuilderContainerTemplate.groovy
@@ -2,8 +2,8 @@ def call() {
 	return [
 		containerTemplate(
 			name: 'node012-builder',
-			image: 'agiledigital/node012-builder',
-	        alwaysPullImage: true,
+			image: 'adelabs/node012-builder',
+	        	alwaysPullImage: false,
 			command: 'cat',
 			ttyEnabled: true
 		)
